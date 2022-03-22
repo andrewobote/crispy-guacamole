27416d3ca1b71eb2d2db18b0d5b7757a7d4b4714
someOneElse <56868117+andrewobote@users.noreply.github.com>
2022-03-22 17:02:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-result = () => {
-    var element = document.createElement('p');
-    var text = document.createTextNode('You did it!');
-    element.appendChild(text);
-    document.querySelector('#result').appendChild(element);
+import people from './data.js'
+
+const Persona = function(data){
+    Object.assign(this, data)
+    const {name, age, city} = this
+    this.info = function(){
+        document.getElementById("root").innerHTML += 
+        `${name} ${age} ${city}<br />`
+    }
+}
+
+for(let person in people){
+    let data = people[person]
+    person = new Persona(data)
+    person.info()
 }
