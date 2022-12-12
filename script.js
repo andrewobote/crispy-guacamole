51ae934da9caf3decca46f5543ded2ec9d9e414f
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2022-12-12 17:21:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function rot13(message) {
-    const lowerCase = [...Array(26)].map((n, i) => String.fromCharCode(97 + i));
-    const upperCase = [...Array(26)].map((n, i) => String.fromCharCode(65 + i));
-    const arr1 = [];
-    message.split("").map((l) => {
-      if (lowerCase.includes(l)) {
-        lowerCase.map((t, i) => {
-          if (l === t) {
-            arr1.push(i);
-          }
-        });
-      } else {
-        upperCase.map((t, i) => {
-          if (l === t) {
-            arr1.push(i);
-          }
-        });
-      }
-    });
-    return arr1
-      .map((n) =>
-        n + 13 > lowerCase.length
-          ? lowerCase[n + 13 - lowerCase.length]
-          : lowerCase[n + 13]
-      )
-      .join("");
-  }
-  rot13("test");
-  
+  const lowerCase = [...Array(26)].map((n, i) => String.fromCharCode(97 + i));
+  const upperCase = [...Array(26)].map((n, i) => String.fromCharCode(65 + i));
+  const arr1 = [];
+  message.split("").map((l) => {
+    if (lowerCase.includes(l)) {
+      lowerCase.map((t, i) => {
+        if (l === t) {
+          arr1.push(i);
+        }
+      });
+    } else {
+      upperCase.map((t, i) => {
+        if (l === t) {
+          arr1.push(i);
+        }
+      });
+    }
+  });
+  return arr1
+    .map((n) =>
+      n + 13 > lowerCase.length
+        ? lowerCase[n + 13 - lowerCase.length]
+        : lowerCase[n + 13]
+    )
+    .join("");
+}
+
+rot13("test");
