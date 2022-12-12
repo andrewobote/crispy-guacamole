dbe8d35e061035d52b1c6e2e7aeaa0ca2670da4f
andrewobote <56868117+andrewobote@users.noreply.github.com>
2022-12-12 21:13:50 +0300

    halfway done

--- script.js
+++ script.js
@@ -3,28 +3,18 @@ function rot13(message) {
   const upperCase = [...Array(26)].map((n, i) => String.fromCharCode(65 + i));
   const arr1 = [];
   message.split("").map((l) => {
-    if (lowerCase.includes(l)) {
-      lowerCase.map((t, i) => {
-        if (l === t) {
-          arr1.push(i);
-        }
-      });
-    } else {
-      upperCase.map((t, i) => {
-        if (l === t) {
-          arr1.push(i);
-        }
-      });
-    }
+    lowerCase.map((t, i) => {
+      if (l === t) {
+        arr1.push(i);
+      }
+    });
   });
   return arr1
-    .map((n) => {
-      if(n + 13 > lowerCase.length) {
-        lowerCase.includes(n) ? lowerCase[n + 13 - lowerCase.length] : upperCase[n + 13 - upperCase.length]
-      } else {
-        lowerCase.includes(n) ? lowerCase[n + 13] : upperCase[n + 13]
-      }
-    })
+    .map((n) =>
+      n + 13 > lowerCase.length
+        ? lowerCase[n + 13 - lowerCase.length]
+        : lowerCase[n + 13]
+    )
     .join("");
 }
 
