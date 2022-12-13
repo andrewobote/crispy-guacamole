9d9772825a8b7165861549805637548919cf17dc
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2022-12-13 17:06:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function rot13(message) {
-    const lowerCase = [...Array(26)].map((n, i) => String.fromCharCode(97 + i));
-    const upperCase = [...Array(26)].map((n, i) => String.fromCharCode(65 + i));
-    const arr1 = [];
-    message.split("").map((l) => {
-      if(lowerCase.includes(l)) {
-          lowerCase.map((t, i) => {
-              if (l === t) {
-                  arr1.push(i);
-              }
-              });
-          } else {
-              upperCase.map((x, y) => {
-                  if (l === x) {
-                      arr1.push(y);
-                  }
-              });            
-          }
-    });
-    return arr1
-      .map((n, y) => {
-        if (message[y] === lowerCase[n]) {
-          return n + 13 > lowerCase.length
-            ? lowerCase[n + 13 - lowerCase.length]
-            : lowerCase[n + 13];
-        } else if (message[y] === upperCase[n]) {
-          return n + 13 > upperCase.length
-            ? upperCase[n + 13 - upperCase.length]
-            : upperCase[n + 13];
+  const lowerCase = [...Array(26)].map((n, i) => String.fromCharCode(97 + i));
+  const upperCase = [...Array(26)].map((n, i) => String.fromCharCode(65 + i));
+  const arr1 = [];
+  message.split("").map((l) => {
+    if (lowerCase.includes(l)) {
+      lowerCase.map((t, i) => {
+        if (l === t) {
+          arr1.push(i);
         }
-      })
-      .join("");
-  }
-  
-  rot13("test"); //grfg
+      });
+    } else {
+      upperCase.map((x, y) => {
+        if (l === x) {
+          arr1.push(y);
+        }
+      });
+    }
+  });
+  return arr1
+    .map((n, y) => {
+      if (message[y] === lowerCase[n]) {
+        return n + 13 > lowerCase.length
+          ? lowerCase[n + 13 - lowerCase.length]
+          : lowerCase[n + 13];
+      } else if (message[y] === upperCase[n]) {
+        return n + 13 > upperCase.length
+          ? upperCase[n + 13 - upperCase.length]
+          : upperCase[n + 13];
+      }
+    })
+    .join("");
+}
+
+rot13("test"); //grfg
