623ee7959a9829b483b404f8c7e2e9b5a64ef94a
andrewobote <56868117+andrewobote@users.noreply.github.com>
2022-12-13 21:12:35 +0300

    done

--- script.js
+++ script.js
@@ -3,33 +3,41 @@ function rot13(message) {
   const upperCase = [...Array(26)].map((n, i) => String.fromCharCode(65 + i));
   const arr1 = [];
   message.split("").map((l) => {
-    if (lowerCase.includes(l)) {
-      lowerCase.map((t, i) => {
-        if (l === t) {
-          arr1.push(i);
-        }
-      });
-    } else if (upperCase.includes(l)) {
-      upperCase.map((x, y) => {
-        if (l === x) {
-          arr1.push(y);
-        }
-      });
+    if (/[a-z]/i.test(l)) {
+      if (lowerCase.includes(l)) {
+        lowerCase.map((t, i) => {
+          if (l === t) {
+            arr1.push(i);
+          }
+        });
+      } else if (upperCase.includes(l)) {
+        upperCase.map((x, y) => {
+          if (l === x) {
+            arr1.push(y);
+          }
+        });
+      }
+    } else {
+      arr1.push(l);
     }
   });
   return arr1
     .map((n, y) => {
-      if (message[y] === lowerCase[n]) {
-        return n + 13 > lowerCase.length
-          ? lowerCase[n + 13 - lowerCase.length]
-          : lowerCase[n + 13];
-      } else if (message[y] === upperCase[n]) {
-        return n + 13 > upperCase.length
-          ? upperCase[n + 13 - upperCase.length]
-          : upperCase[n + 13];
+      if (typeof n === "number") {
+        if (message[y] === lowerCase[n]) {
+          return n + 13 >= lowerCase.length
+            ? lowerCase[n + 13 - lowerCase.length]
+            : lowerCase[n + 13];
+        } else if (message[y] === upperCase[n]) {
+          return n + 13 >= upperCase.length
+            ? upperCase[n + 13 - upperCase.length]
+            : upperCase[n + 13];
+        }
+      } else {
+        return n;
       }
     })
     .join("");
 }
 
-rot13("test"); //grfg
+rot13("test");
