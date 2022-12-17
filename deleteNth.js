eb3b461f180909fe2aef06cead63279fe52aa1af
andrewobote <56868117+andrewobote@users.noreply.github.com>
2022-12-18 00:13:18 +0300

    regex

--- deleteNth.js
+++ deleteNth.js
@@ -1,5 +1,18 @@
+// function deleteNth(arr, n) {
+//   const duplicates = {};
+//   const arr1 = []
+//   arr.map((n, i) => (duplicates[n] = (duplicates[n] || 0) + 1));
+//   arr.map(y => {
+//       if (!arr1.includes(y) || duplicates[y] < n) {
+//         arr1.push(y)
+//       } 
+//     }) 
+
+//   return arr1
+// }
+// deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3); //[1, 1, 3, 3, 7, 2, 2, 2]
+
 function deleteNth(arr, n) {
-  const duplicates = {};
-  arr.map((n, i) => (duplicates[n] = (duplicates[n] || 0) + 1));
+  const reg = new RegExp(`\\d{${n - 1}}`, "g");
+  return arr.join("").match(reg).join("").split("").map(Number);
 }
-deleteNth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3); //[1, 1, 3, 3, 7, 2, 2, 2]
