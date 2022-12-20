c0963fe186f5e611a0a04bfa55862c8dc99bced7
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2022-12-20 14:17:37 +0300

    ok

--- incrementString.js
+++ incrementString.js
@@ -1,21 +1,21 @@
 function incrementString(strng) {
-    if (!strng.match(/\d/g) || !strng.match(/[^\d]/g)) {
-        return strng.match(/\d/g) ? Number(strng) + 1 : strng + "1"
-    } else {
-      const numArr = strng.match(/\d/g);
-      let zeros = 0;
-      numArr.map((n) => {
-        if (n === "0") {
-          zeros += 1;
-        }
-      });
-      const numStr = numArr.join("");
-      const num = Number(numStr) + 1;
-        console.log(Number(numStr))
-      const str = strng.match(/[^\d]/g).join("");
-      const strLength = Number(numStr) === 0 ? str.length - 1 : str.length  
-      const strPad = str.padEnd(strLength + zeros, "0");
-      return strPad + num;
-    } 
+  if (!strng.match(/\d/g) || !strng.match(/[^\d]/g)) {
+    return strng.match(/\d/g) ? Number(strng) + 1 : strng + "1";
+  } else {
+    const numArr = strng.match(/\d/g);
+    let zeros = 0;
+    numArr.map((n) => {
+      if (n === "0") {
+        zeros += 1;
+      }
+    });
+    const numStr = numArr.join("");
+    const num = Number(numStr) + 1;
+    console.log(Number(numStr));
+    const str = strng.match(/[^\d]/g).join("");
+    const strLength = Number(numStr) === 0 ? str.length - 1 : str.length;
+    const strPad = str.padEnd(strLength + zeros, "0");
+    return strPad + num;
   }
-  incrementString("foobar00999") //"foobar01000"
+}
+incrementString("foobar00999"); //"foobar01000"
