52355992cfaa5e12f41cdedb1b64b661f24d96d4
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2022-12-20 16:32:11 +0300

    working

--- incrementString.js
+++ incrementString.js
@@ -1,6 +1,6 @@
 function incrementString(strng) {
   if (!strng.match(/\d/g) || !strng.match(/[^\d]/g)) {
-    return strng.match(/\d/g) ? Number(strng) + 1 : strng + "1";
+    return strng.match(/\d/g) ? `${Number(strng) + 1}` : strng + "1";
   } else {
     const numArr = strng.match(/\d/g);
     let zeros = 0;
@@ -11,11 +11,16 @@ function incrementString(strng) {
     });
     const numStr = numArr.join("");
     const num = Number(numStr) + 1;
-    console.log(Number(numStr));
+    const numLength = num.toString().length;
     const str = strng.match(/[^\d]/g).join("");
-    const strLength = Number(numStr) === 0 ? str.length - 1 : str.length;
+    const strLength =
+      Number(numStr) === 0 || numArr[numArr.length - 1] === "9"
+        ? str.length - 1
+        : str.length;
     const strPad = str.padEnd(strLength + zeros, "0");
     return strPad + num;
   }
 }
-incrementString("foobar00999"); //"foobar01000"
+//incrementString("foobar001") //"foobar002"
+//incrementString("foobar00999"); //"foobar01000"
+incrementString("009"); //"010"
