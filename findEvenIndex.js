d0fa70423e765997df4908e111ab82b1c6c37109
andrewobote <56868117+andrewobote@users.noreply.github.com>
2022-12-21 23:33:45 +0300

    commit

--- findEvenIndex.js
+++ findEvenIndex.js
@@ -1,11 +1,25 @@
 function findEvenIndex(arr) {
-    const leftArr = arr.map((n, i) => arr.slice(i + 1))
-    const left = leftArr.map((x, y) => {if(x.length !== 0){return x.reduce((a, b) => a + b)}})
-    const rightArr = arr.reverse().map((n, i) => arr.slice(i + 1))
-    const right = rightArr.map((x, y) => {if(x.length !== 0){return x.reduce((a, b) => a + b)}})
-    console.log(leftArr)
-    console.log(left)
-    console.log(rightArr)
-    console.log(right)
-}
-findEvenIndex([6,1,1,3,2]) //3
+    const arr1 = [];
+    const leftArr = arr.map((n, i) => arr.slice(i));
+    const left = leftArr.map((x, y) => {
+      if (x.length !== 0) {
+        return x.reduce((a, b) => a + b);
+      }
+    });
+    const rightArr = arr.reverse().map((n, i) => arr.slice(i));
+    const right = rightArr
+      .map((x, y) => {
+        if (x.length !== 0) {
+          return x.reduce((a, b) => a + b);
+        }
+      })
+      .reverse();
+    for (let i = 0; i < left.length; i++) {
+      if (left[i] === right[i]) {
+        arr1.push(left.indexOf(left[i]));
+      }
+    }
+    return arr1.length === 0 ? -1 : arr1[0];
+  }
+  findEvenIndex([1, 2, 3, 4, 5, 6]);
+  
