170646b851dac2a50c2b271506742cf49e0ed6dd
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2022-12-23 16:42:23 +0300

    done

--- incrementString.js
+++ incrementString.js
@@ -1,35 +1,11 @@
 function incrementString(strng) {
-  if (!strng.match(/\d/g) || !strng.match(/[^\d]/g)) {
-    //return strng.match(/\d/g) ? `${Number(strng) + 1}` : strng + "1";
-    if (strng.match(/\d/g)) {
-      //return `${Number(strng) + 1}`
-      const zero = strng.match(/^[0]+/g).join();
-      //console.log(zero.length);
-      const zeroLength = strng[strng.length - 1] === "9" ? zero.length - 2 : zero.length;
-      const addNum = `${Number(strng) + 1}`;
-      return addNum.padStart(strng.length + zeroLength, "0");
-    } else {
-      return strng + "1";
-    }
-  } else {
-    const numArr = strng.match(/\d/g);
-    let zeros = 0;
-    numArr.map((n) => {
-      if (n === "0") {
-        zeros += 1;
-      }
-    });
-    const numStr = numArr.join("");
-    const num = Number(numStr) + 1;
-    //const numLength = num.toString().length;
-    const str = strng.match(/[^\d]/g).join("");
-    const strLength =
-      Number(numStr) === 0 || numArr[numArr.length - 1] === "9"
-        ? str.length - 1
-        : str.length;
-    const strPad = str.padEnd(strLength + zeros, "0");
-    return strPad + num;
-  }
+  const digits = strng.match(/[0-9]+$/) ? strng.match(/[0-9]+$/) : 0;
+  const textLength = digits ? strng.length - digits[0].length : strng.length;
+  const text = digits ? strng.slice(0, textLength) : strng;
+  const num = Number(digits) + 1;
+  return `${text}${num.toString().padStart(strng.length - text.length, "0")}`;
 }
+incrementString("asd"); //"asd1"
+//incrementString("asd300") //"asd301"
 //incrementString("1") //"2"
-incrementString("fo99obar99") //"fo99obar100"
+//incrementString("fo99obar99") //"fo99obar100"
