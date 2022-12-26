0fab05f02ab85e692795ac1ed6e49ead9a9b7516
andrewobote <56868117+andrewobote@users.noreply.github.com>
2022-12-26 21:03:01 +0300

    done

--- diamond.js
+++ diamond.js
@@ -7,14 +7,13 @@ function diamond(n) {
     }
     const arr2 = [...arr1, ...arr1.reverse().slice(1)];
     const arr3 = [];
-    for (let i = n - 1; i >= 0; i -= 1) {
+    for (let i = Math.floor(n / 2); i >= 0; i -= 1) {
       arr3.push(i);
     }
     const arr4 = [...arr3.slice(0, -1), ...arr3.reverse()];
-    arr2.map((n, i) => (str += " ".repeat(arr4[i]) + "*".repeat(n) + "\n"));
+    arr2.map((y, i) => (str += " ".repeat(arr4[i]) + "*".repeat(y) + "\n"));
     return str.length === 2 ? str.slice(0, -1) : str;
   } else {
     return null;
   }
 }
-
