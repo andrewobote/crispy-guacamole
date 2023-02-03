03e812cc0c31d19e9b0f3b74bdcc438aefdedb6d
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2023-02-03 13:41:28 +0300

    commit

--- nico.js
+++ nico.js
@@ -4,7 +4,9 @@ function nico(key, message) {
   for (let i = 0; i < key.length; i++) {
     arr2.push(arr1.indexOf(key[i]) + 1);
   }
-  return arr2;
+  const regex = new RegExp(`.{1,${key.length}}`, "g");
+  const arr3 = message.match(regex);
+  console.log(arr2);
+  console.log(arr3);
 }
 nico("abc", "abcd");
-
