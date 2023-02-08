50c3d7cafa2ddf94238235436d994e200959f319
SomeOneElse <56868117+andrewobote@users.noreply.github.com>
2023-02-08 16:19:25 +0300

    done

--- nico.js
+++ nico.js
@@ -27,3 +27,25 @@ function nico(key, message) {
   return result.join("");
 }
 nico("crazy", "secretinformation"); //"cseerntiofarmit on  "
+
+function deNico(key, message) {
+  const arr1 = key.split("").sort();
+  const arr2 = [];
+  for (let i = 0; i < key.length; i++) {
+    arr2.push(arr1.indexOf(key[i]) + 1);
+  }
+  const regex = new RegExp(`.{1,${key.length}}`, "g");
+  const arr3 = message.match(regex);
+  const arr4 = [];
+  arr3.map((a) => arr4.push(a.split("")));
+  const arr5 = [];
+  for (let y = 0; y < arr4.length; y++) {
+    arr5.push(new Array());
+    for (let i = 0; i < arr2.length; i++) {
+      arr5[y][i] = arr4[y][arr2[i] - 1];
+    }
+  }
+  const result = arr5.map((a) => a.join(""));
+  return result.join("").trimEnd();
+}
+deNico("crazy", "cseerntiofarmit on  ");
